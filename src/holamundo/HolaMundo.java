16f174e6e5573d29ddd629d2c7e21486f8a973cc
MariGutierrez <malymiku@gmail.com>
2023-02-22 23:54:08 -0600

    Esta es mi primer versión

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+/**
+ *
+ * @author kika
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
